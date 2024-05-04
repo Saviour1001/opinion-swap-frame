7b29972e1cdab32ad01c824bd6d5eb336073b91b
Saviour1001 <harshghodkar@gmail.com>
2024-05-05 07:31:17 +1000

    nit

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,7 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: "Frames Next.js Example",
     other: {
-      ...(await fetchMetadata(
-        new URL("/frames", process.env.VERCEL_URL || "http://localhost:3000")
-      )),
+      ...(await fetchMetadata(new URL("/frames", process.env.HOST_URL))),
     },
   };
 }
